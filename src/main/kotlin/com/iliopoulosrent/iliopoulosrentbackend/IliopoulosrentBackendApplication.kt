e423b6ab3c4e0e1c15941ed883fe732ad0c29b83
nikolasil <nikolasil2000@gmail.com>
2024-02-13 11:01:08 +0200

    Initial commit

---
+++ src/main/kotlin/com/iliopoulosrent/iliopoulosrentbackend/IliopoulosrentBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.iliopoulosrent.iliopoulosrentbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IliopoulosrentBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<IliopoulosrentBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/iliopoulosrent/iliopoulosrentbackend/IliopoulosrentBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iliopoulosrent.iliopoulosrentbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IliopoulosrentBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
